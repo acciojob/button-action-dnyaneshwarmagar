51b52cf47b76fb88c5d1a3b1798326c394275ea8
dnyaneshwarmagar <dnyaneshwarmagar276@gmail.com>
2024-04-30 00:46:48 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ const App = (props) => {
   return (
     <div className="App" id="main">
       
-      <p id="para" className={visible?"show":"hide"}>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"</p>
+      <p id="para" className={visible?"show":"hide"}>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
       <button id="click" onClick={()=>{setVisible(!visible)}}>{visible?"Hide":"Show"}</button>
     </div>
   );
